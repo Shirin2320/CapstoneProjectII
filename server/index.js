18c3bc5ca77bf33ab2c0a6b9e62ad8fdd3ca3cf1
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-01-26 16:39:37 -0500

    Use nodemon to start the server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3123;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("COMP 3078 Capstone Project"));
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
 export default app;
