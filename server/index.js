96408ef86d5a1df342d1ab9fb57d4a94ae4d5636
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-01-26 20:27:02 -0500

    Use sequelize to manage database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 
 const app = express();
+const sequelize = new Sequelize('postgres://komedb:komepass7@localhost:5433/kome');
 const PORT = process.env.PORT || 3123;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("COMP 3078 Capstone Project"));
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+sequelize.authenticate()
+    .then(_ => app.listen(PORT, () => console.log(`Server ready on port ${PORT} with PostgreSQL.`)))
+    .catch(error => console.log(error));
 
 export default app;
