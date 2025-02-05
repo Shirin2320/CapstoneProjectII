c60196678d5c9d6c27881089c01db1feedff214e
n2milmin <nicoleamilmine@gmail.com>
2025-02-05 08:13:13 -0500

    Dietary Restrictions model & interface

---
+++ server/src/Interface/Dietary_Restrictions.ts
@@ -0,0 +1,7 @@
+interface IDietary_Restrictions {
+    id?: number,
+    name: string,
+    user_ids: []
+}
+
+export default IDietary_Restrictions;
---
+++ server/src/model/dietary_restrictions.ts
@@ -0,0 +1,31 @@
+import { BelongsTo, Column, DataType, Model, Table} from "sequelize-typescript";
+import IDietary_Restrictions from "../Interface/Dietary_Restrictions";
+import User from "./user.js"
+
+@Table({
+    tableName: "dietary restrictions",
+    timestamps: false,
+})
+
+export default class Dietary_Restrictions extends Model<Dietary_Restrictions> implements IDietary_Restrictions {
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    id!: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    name!: string;
+
+    @Column({
+        type: DataType.ARRAY(DataType.INTEGER),
+        allowNull: true
+    })
+    user_ids!: []
+
+    @BelongsTo(() => User, "user_ids") user!: User;
+}
