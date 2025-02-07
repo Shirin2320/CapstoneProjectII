16725255459a233b3e31ea112bf4fbc3d6a1bd1c
n2milmin <nicoleamilmine@gmail.com>
2025-02-07 14:11:41 -0500

    Added models to db creation/call

--- server/src/database.js
+++ server/src/database.js
@@ -3,7 +3,15 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('postgres://komedb:komepass7@localhost:5433/kome');
 
 const models = [
-    require('./model/user')
+    require('./model/user'),
+    require("./model/allergies"),
+    require("./model/answers"),
+    require("./model/categories"),
+    require("./model/dietary_restrictions"),
+    require("./model/ingredient"),
+    require("./model/meal_history"),
+    require("./model/questionnaire"),
+    require("./model/recipe")
 ];
 
 for (const model of models) {
