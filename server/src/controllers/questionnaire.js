d4e60fd4005529977705e409cb9d1a1f9827b044
n2milmin <nicoleamilmine@gmail.com>
2025-03-05 15:58:09 -0500

    Created a call to the QuestionnaireCategory table to collect 4 questions & their categories based on 4 randomly generated, non-repeating numbers

--- server/src/controllers/questionnaire.js
+++ server/src/controllers/questionnaire.js
@@ -1,2 +1,25 @@
 const { models } = require('../database');
 
+const questionnaireController = {
+    getQuestionsById: async () => {
+        const questions = []
+        const ids = []
+        
+        // 4 random, non-repeating numbers 
+        do {
+            let num = Math.floor(Math.random() * 12)
+            let check = ids.includes(num)
+            if(!check)
+                ids.push(num)
+        } while(ids.length < 4)
+
+        // Get corresponding questions & answers
+        for(i=0; i<4; i++){
+            questions[i] = await models.QuestionnaireCategory.find({ QuestionnaireId: ids[i] })
+        }
+    
+        return questions
+    }
+}
+
+module.exports = questionnaireController;
