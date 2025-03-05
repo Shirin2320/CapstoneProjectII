d9f8dd71c7e6e4acc27f922fce6c3373cb963bb8
n2milmin <nicoleamilmine@gmail.com>
2025-03-05 11:27:12 -0500

    Removed Answers.js b/c it was just this weird intermediary between questions & categories so now we just use categories

--- server/src/database.js
+++ server/src/database.js
@@ -4,7 +4,6 @@ const sequelize = new Sequelize('postgres://komedb:komepass7@localhost:5433/kome
 
 const models = [
     require("./model/allergies"),
-    require("./model/answers"),
     require("./model/categories"),
     require("./model/dietary_restrictions"),
     require("./model/ingredient"),
