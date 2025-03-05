ba4d332da981d3c1b46c1797295d1218921b51e9
n2milmin <nicoleamilmine@gmail.com>
2025-03-05 11:23:09 -0500

    added get by id to questionnaire router

--- server/src/routes/questionnaire.js
+++ server/src/routes/questionnaire.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { models } = require('../database');
 
-
 router.get('/', async (req, res) => {
     try{
         const response = await models.Questionnaire.findAll()
         res.send(response) 
 
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const response = await models.Questionnaire.findById(id)
+        
+        res.send(response) 
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
