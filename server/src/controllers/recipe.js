8f5b7effe3fd757079f4b8d2df8c65f6cfd820b2
n2milmin <nicoleamilmine@gmail.com>
2025-03-05 16:14:05 -0500

    Added a recipe controller that will handle creating, updating & deleting recipes

---
+++ server/src/controllers/recipe.js
@@ -0,0 +1,13 @@
+const { models } = require('../database');
+
+const recipeController = {
+    createRecipe: async (userId, recipe) => {
+
+    },
+    updateRecipe: async (userId, recipe) => {
+
+    },
+    deleteRecipe: async (userId, recipeId) => {
+        
+    }
+}
