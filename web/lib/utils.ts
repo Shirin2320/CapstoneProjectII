0af6d61c26cdef8d930a501fddc8ac34dfc48f82
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-03-09 13:20:21 -0400

    Add environment file for server config

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getServerURL() {
+  return process.env.NEXT_PUBLIC_ENV === "development"
+    ? "http://localhost:3123"
+    : process.env.NEXT_PUBLIC_SERVER_URL;
+}
+
+export function buildURL(route: string) {
+  return getServerURL() + route;
+}
